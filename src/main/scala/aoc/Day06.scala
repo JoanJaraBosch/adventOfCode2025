25cdc2caf8fe77e5f8b9d6ead0b8792208c58654
Joan Jara Bosch <jaraboschj@gmail.com>
2025-12-07 22:15:02 +0100

    day 6 and 7

--- src/main/scala/aoc/Day06.scala
+++ src/main/scala/aoc/Day06.scala
@@ -4,17 +4,76 @@ import util.Input
 
 object Day06 {
 
-  def part1(input: List[String]): Int = {
+  def part1(operands: List[String],numbers: List[List[String]]): Long = {
+    var cont = 0
+    var result: Long =0
+    var loopProd: Long = 1
+    var loopSuma: Long = 0
+
+    operands.foreach{
+      operand =>
+        numbers.foreach{
+          list =>
+            if(operand == "*") loopProd = loopProd * list(cont).toLong
+            else loopSuma = loopSuma + list(cont).toLong
+        }
+        if(loopSuma == 0 && loopProd != 1) result = result + loopProd
+        else result = result + loopSuma
+
+        loopProd = 1
+        loopSuma = 0
+        cont = cont + 1
+    }
+
+    result
+  }
+
+  def part2(lines: List[String]): BigInt = {
+    //No ho entenia, la part2 es copiada
     0
   }
 
-  def part2(input: List[String]): Int = {
-    0
+  def evaluate(expr: List[String]): Long = {
+    val operands = expr.init.map(_.toLong)
+    expr.last match{
+      case "+" => operands.sum
+      case "*" => operands.product
+    }
+  }
+
+  def partOne(input: List[String]): Long = {
+    input.map(_.trim.split(" +")).transpose.map(evaluate).sum
+  }
+
+  def partTwo(input: List[String]): Long = {
+    def groupColumns(cols: List[List[Char]]): List[List[List[Char]]] = {
+      if(cols.isEmpty) Nil
+      else {
+        val (group, rest) = cols.span(_.exists(_.isDigit))
+        group :: groupColumns (rest.drop (1) )
+      }
+
+    }
+    def processGroup(group: List[List[Char]]): List[String] = {
+      val numbers = group.map(_.init.mkString.trim)
+      val operator = group.head.last.toString
+      numbers :+ operator
+    }
+      val maxLen = input.map(_.length).max
+      val cols = input.map(_.padTo(maxLen, ' ').toList).transpose
+      groupColumns(cols).map(processGroup).map(evaluate).sum
   }
 
   def main(args: Array[String]): Unit = {
-    val data = Input.readLines(6)
-    println(s"Part 1: ${part1(data)}")
-    println(s"Part 2: ${part2(data)}")
+    val raw = Input.readLines(6).toList
+    val data = raw.map(_.trim).filter(_.nonEmpty)
+    val operands = data.last.split("\\s+").toList
+
+    val numbers = data.init.map(_.split("\\s+").toList)
+    println(s"Part 1: ${part1(operands, numbers)}")
+    println(s"Part 1 optimized: ${partOne(raw)}")
+    println(s"Part 2: ${part2(raw)}")
+    //https://github.com/lupari/aoc2025/blob/main/src/main/scala/assignments/Day06.scala
+    println(s"Part 2 credits to lupari: ${partTwo(raw)}")
   }
 }
--- src/main/scala/aoc/Day07.scala
+++ src/main/scala/aoc/Day07.scala
@@ -1,19 +1,96 @@
 package aoc
 
 import util.Input
+import scala.collection.mutable
 
 object Day07 {
 
-  def part1(input: List[String]): Int = {
-    0
+  private case class Beam(row: Int, col: Int)
+
+  def part1(lines: List[String]): Int = {
+    val rows = lines.length
+    val cols = lines.head.length
+    val grid = lines.map(_.toCharArray).toArray
+
+    // Buscar la Beamición de S
+    val startCol = grid(0).indexOf('S')
+    var activeBeams = mutable.Set(Beam(0, startCol))
+    var splits = 0
+
+    while (activeBeams.nonEmpty) {
+      val nextBeams = mutable.Set.empty[Beam]
+
+      for (beam <- activeBeams) {
+        val r = beam.row + 1
+        val c = beam.col
+
+        if (r < rows) {
+          grid(r)(c) match {
+            case '.' =>
+              nextBeams += Beam(r, c)  // sigue hacia abajo
+            case '^' =>
+              splits += 1
+              if (c > 0) nextBeams += Beam(r, c - 1) // izquierda
+              if (c < cols - 1) nextBeams += Beam(r, c + 1) // derecha
+            case _ => // no hacer nada
+          }
+        }
+      }
+
+      activeBeams = nextBeams
+    }
+
+    splits
   }
 
-  def part2(input: List[String]): Int = {
-    0
+  def part2(lines: List[String]): Long = {
+    val rows = lines.length
+    val cols = lines.head.length
+    val grid = lines.map(_.toCharArray).toArray
+
+    // Buscar la posición de S
+    val startCol = grid(0).indexOf('S')
+    var active = mutable.Map(Beam(0, startCol) -> 1L)
+    var totalTimelines: Long = 0
+
+    while (active.nonEmpty) {
+      val next = mutable.Map.empty[Beam, Long]
+
+      for ((beam, count) <- active) {
+        val r = beam.row + 1
+        val c = beam.col
+
+        if (r >= rows) {
+          // El haz salió del mapa
+          totalTimelines += count
+        } else {
+          grid(r)(c) match {
+            case '.' =>
+              val existing = next.getOrElse(Beam(r, c), 0L)
+              next(Beam(r, c)) = existing + count
+            case '^' =>
+              // Cada timeline se divide en dos
+              if (c > 0) {
+                val existingL = next.getOrElse(Beam(r, c - 1), 0L)
+                next(Beam(r, c - 1)) = existingL + count
+              }
+              if (c < cols - 1) {
+                val existingR = next.getOrElse(Beam(r, c + 1), 0L)
+                next(Beam(r, c + 1)) = existingR + count
+              }
+            case _ => // nada
+          }
+        }
+      }
+
+      active = next
+    }
+
+    totalTimelines
   }
 
   def main(args: Array[String]): Unit = {
-    val data = Input.readLines(7)
+    val data = Input.readLines(7).map(_.trim).filter(_.nonEmpty)
     println(s"Part 1: ${part1(data)}")
     println(s"Part 2: ${part2(data)}")
   }
